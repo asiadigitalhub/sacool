265b93e196ebf415bb0ab7d6e4d41927492f7bf1
thanhtung <thanhtung.nguyen@ogilvy.com>
2022-03-02 16:50:14 +0700

    Set Quality Material as Medium by Default

--- src/react-components/preferences-screen.js
+++ src/react-components/preferences-screen.js
@@ -1080,7 +1080,7 @@ class PreferencesScreen extends Component {
                 })
               }
             ],
-            // defaultString: 'defaultMaterialQualitySetting',
+            // defaultString: 'defaultMaterialQualitySetting', 
             defaultString: 'medium',
             promptForRefresh: true
           },
