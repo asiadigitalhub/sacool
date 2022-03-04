5fcd96fc7567d86ffcccecc707e698e46f8103c9
thanhtung <thanhtung.nguyen@ogilvy.com>
2022-03-04 10:48:00 +0700

    open an Dialog to guide Block Pop-ups

--- src/react-components/home/CreateRoomButton.js
+++ src/react-components/home/CreateRoomButton.js
@@ -14,28 +14,30 @@ export function CreateRoomButton() {
       preset="landing"
       onClick={e => {
         e.preventDefault();
-        const title = intl.formatMessage({id: "popup-safari", defaultMessage: "Please open window blocking in safari: Settings > Safari > Pop-up Blocker" });
+        const tag = document.createElement("div")
+        tag.classList.add('popupSafari')
+        const add = "<p class='close'> " 
+        const add1 = "</p>"
+        const para = document.createElement("div");
+        para.classList.add('popupSafari')
+        
         if(/constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification)) == true){
-          const tag = document.createElement("div")
-          tag.classList.add('popupSafari')
-          const add = "<p class='close'> " 
-          const add1 = "</p>"
-          const para = document.createElement("div");
-          para.classList.add('popupSafari')
+          const title = intl.formatMessage({id: "popup-safari", defaultMessage: "To open this hyperlink,  Unblock Pop-ups in your Safari settings. (Setting > Safari > Uncheck Block Pop-ups)" });
           const notifyText = title;
           para.innerHTML = add + notifyText + add1;
           document.querySelector('body').appendChild(para);
-          setTimeout(function() {
-            document.querySelector('.popupSafari .close').addEventListener('click', (e) => {
-              document.querySelector('.popupSafari').classList.add('hidden');
-              createAndRedirectToNewHub(null, null, false);
-            });
-
-          }, 1000); 
         } else{
-          createAndRedirectToNewHub(null, null, false);
+          const title = intl.formatMessage({id: "popup-all-browse", defaultMessage: "To open this hyperlink, make sure to not block Pop-ups in your Browser settings" });
+          const notifyText = title;
+          para.innerHTML = add + notifyText + add1;
+          document.querySelector('body').appendChild(para);
         }
-        // createAndRedirectToNewHub(null, null, false);
+        setTimeout(function() {
+          document.querySelector('.popupSafari .close').addEventListener('click', (e) => {
+            document.querySelector('.popupSafari').classList.add('hidden');
+            createAndRedirectToNewHub(null, null, false);
+          });
+        }, 1000); 
         
       }}
     >
