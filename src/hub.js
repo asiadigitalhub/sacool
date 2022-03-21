bbe322461aebf3b20de501a35db9743ac91952da
DoHungCuong <cuongdo@gmail.com>
2022-03-21 10:24:39 +0700

    Number of user in room includes lobby + user in room

--- src/hub.js
+++ src/hub.js
@@ -1405,14 +1405,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   events.on(`hub:sync`, ({ presence }) => {
     // update number of user in the room hubId in firebase
     if (presence.state) {
-      var numberOfUser = 0;
-      for (var peopleKey in presence.state) { 
-        // if user is entering or in room, then increase the number of user
-        if (presence.state[peopleKey].metas != null && presence.state[peopleKey].metas.length > 0 && 
-          presence.state[peopleKey].metas[0].presence == "room" || presence.state[peopleKey].presence == "entering") {
-          numberOfUser += 1; // increase by 1
-        }
-      } 
+      var numberOfUser = Object.keys(presence.state).length;     
       // update in firebase
       setNumberOfUserInRoom(hubId, numberOfUser);
     }    
