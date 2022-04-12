8db9909d22604d1ea0b4e783533b5581f0f9d791
nhatduy.tran <tranduy2610@gmail.com>
2022-04-12 09:27:20 +0700

    update Social Style

--- src/react-components/room/ChatSidebar.js
+++ src/react-components/room/ChatSidebar.js
@@ -345,12 +345,15 @@ function getMessageComponent(message, sent) {
       );
     }
     case "video":
+      const urlSrc = 'https://forward-camera-345608.web.app/shareVideo?src='+message.body.src;
+      var player;
       return (
         <MessageBubble key={message.id} media>
-          <video controls src={message.body.src} />
+          {/* <video controls src={message.body.src} preload="metadata" onLoad={()=>{this.player.seek(0)}}/> */}
+          <video controls src={message.body.src} paused={true} ref={(ref)=>{player=ref}} onLoad={()=>{player.seek(1)}} />
           <div className={styles.socialShareContainer}>
-            <div class="fb-share-button" data-href={message.body.src} data-layout="button" data-size="small"><a target="_blank" href={message.body.src} class="fb-xfbml-parse-ignore">Chia sẻ</a></div>
-            <div class="zalo-share-button" data-href={message.body.src} data-oaid="579745863508352884" data-layout="2" data-color="blue" data-customize="false"></div>
+            <div class="fb-share-button" data-href={urlSrc}  data-layout="button" data-size="small"><a target="_blank" href={message.body.src} class="fb-xfbml-parse-ignore">Chia sẻ</a></div>
+            <div class="zalo-share-button" data-href={urlSrc} data-oaid="579745863508352884" data-layout="2" data-color="blue" data-customize="false"></div>
           </div>
         </MessageBubble>
       )
