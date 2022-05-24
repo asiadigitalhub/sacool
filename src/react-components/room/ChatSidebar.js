8bff5de74359378bc8b6cda0c6f452f0f60f152e
nhatduy.tran <tranduy2610@gmail.com>
2022-05-24 10:47:28 +0700

    Update ChatSidebar.js

--- src/react-components/room/ChatSidebar.js
+++ src/react-components/room/ChatSidebar.js
@@ -351,7 +351,8 @@ function getMessageComponent(message, sent) {
           {/* <video controls src={message.body.src} preload="metadata" onLoad={()=>{this.player.seek(0)}}/> */}
           <video controls src={message.body.src} paused={true} />
           <div className={styles.socialShareContainer}>
-            <div class="fb-share-button" data-href={urlSrc}  data-layout="button" data-size="small"><a target="_blank" href={message.body.src} class="fb-xfbml-parse-ignore">Chia sẻ</a></div>
+            {/* <div class="fb-share-button" data-href={urlSrc}  data-layout="button" data-size="small"><a target="_blank" href={message.body.src} class="fb-xfbml-parse-ignore">Chia sẻ</a></div> */}
+            <div class="facebook-share-button" onClick={() => {fbShare(urlSrc)}}><svg fill="#ffffff" viewBox="0 0 24 24" width="16px" height="16px"><path d="M17.525,9H14V7c0-1.032,0.084-1.682,1.563-1.682h1.868v-3.18C16.522,2.044,15.608,1.998,14.693,2 C11.98,2,10,3.657,10,6.699V9H7v4l3-0.001V22h4v-9.003l3.066-0.001L17.525,9z"/></svg> Chia sẻ</div>
             <div  onClick={() => {
               logSocialShare({
                 method: "zalo",
